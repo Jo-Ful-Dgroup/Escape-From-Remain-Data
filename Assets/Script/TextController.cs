8e02ad540011e78b838453a60caf4ad401c1f4bb
岩井洋貴 <oct18able@gmail.com>
2018-12-26 03:01:04 +0900

    キャラクタの画像なし版完成

---
+++ Assets/Script/TextController.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextController : MonoBehaviour
+{
+
+
+
+    public string[] scenarios;
+    [SerializeField] Text uiText;
+
+    [SerializeField]
+    [Range(0.001f, 0.3f)]
+    float intervalForCharacterDisplay = 0.05f;
+
+    private string currentText = string.Empty;
+    private float timeUntilDisplay = 0;
+    private float timeElapsed = 1;
+    private int currentLine = 0;
+    private int lastUpdateCharacter = -1;
+
+    public GameObject hekiga_suuhai;
+    public GameObject hekiga_hanran;
+
+    private int changeCounter = 0;
+
+    // 文字の表示が完了しているかどうか
+    public bool IsCompleteDisplayText
+    {
+        get { return Time.time > timeElapsed + timeUntilDisplay; }
+    }
+
+
+    void Start()
+    {
+        SetNextLine();
+    }
+
+
+    void Update()
+    {
+        // 文字の表示が完了してるならクリック時に次の行を表示する
+        if (IsCompleteDisplayText)
+        {
+            if (currentLine < scenarios.Length && Input.GetMouseButtonDown(0))
+            {
+                SetNextLine();
+            }
+        }
+        else
+        {
+            // 完了してないなら文字をすべて表示する
+            if (Input.GetMouseButtonDown(0))
+            {
+                timeUntilDisplay = 0;
+            }
+        }
+
+        int displayCharacterCount = (int)(Mathf.Clamp01((Time.time - timeElapsed) / timeUntilDisplay) * currentText.Length);
+        if (displayCharacterCount != lastUpdateCharacter)
+        {
+            uiText.text = currentText.Substring(0, displayCharacterCount);
+            lastUpdateCharacter = displayCharacterCount;
+        }
+    }
+
+    void SetNextLine()
+    {
+        currentText = scenarios[currentLine];
+        timeUntilDisplay = currentText.Length * intervalForCharacterDisplay;
+        timeElapsed = Time.time;
+        currentLine++;
+        lastUpdateCharacter = -1;
+
+        ImageChange(changeCounter);
+        changeCounter++;
+
+    }
+
+    void ImageChange(int changenumber)
+    {
+        if (changeCounter == 4)
+        {
+            hekiga_suuhai.SetActive(true);
+        }
+        if (changeCounter == 6)
+        {
+            hekiga_hanran.SetActive(true);
+        }
+        if (changeCounter == 10)
+        {
+            hekiga_hanran.SetActive(false);
+            hekiga_suuhai.SetActive(false);
+        }
+
+    }
+
+
+
+}
