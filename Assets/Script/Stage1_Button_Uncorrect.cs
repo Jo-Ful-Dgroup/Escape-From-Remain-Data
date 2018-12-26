a783b74551e907d84314bfcab59d273c01cce107
岩井洋貴 <oct18able@gmail.com>
2018-12-26 17:10:02 +0900

    音無、キャラ画像なし版完成

---
+++ Assets/Script/Stage1_Button_Uncorrect.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stage1_Button_Uncorrect : MonoBehaviour
+{
+
+    public GameObject uncorrect_img;
+    public GameObject hintButton;
+
+    public void OnClick()
+    {
+        uncorrect_img.SetActive(true);
+        Invoke("ImgReset", 1.0f);
+
+    }
+
+    void ImgReset()
+    {
+        uncorrect_img.SetActive(false);
+        hintButton.SetActive(true);
+    }
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
